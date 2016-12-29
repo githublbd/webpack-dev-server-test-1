82240418bd20078251c600ff926fc4c3a3bf5ce9
suifengfengye <1527261080@qq.com>
2016-12-29 23:53:01 +0800

    testing submit

--- app/main.js
+++ app/main.js
@@ -1,2 +1,3 @@
 
 document.write("why I cannot learn webpack well? 我的天，怎么回事？word天~~~");
+document.write('<br/>hello, why ???');
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,11 +1,20 @@
 var path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: {
-    app: ["./app/main.js"]
+    // app: ["./app/main.js"]
+    app: "./app/main.js"
   },
   output: {
     path: path.resolve(__dirname, "../build"),
     publicPath: "/assets/",
     filename: "bundle.js"
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'This is greate',
+      template: './app/template.html',
+      filename: 'app.html'
+    })
+  ]
 };
